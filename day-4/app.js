7414aa51abb4e48bafe5def4c6c95befece7eb9e
APJ-9 <akhil.jose@vonnue.com>
2022-09-06 23:03:42 +0530

    javaScript tutorials

--- day-4/app.js
+++ day-4/app.js
@@ -55,3 +55,32 @@ console.log(myStr);
 strLen = myStr.length;
 console.log(strLen);
 console.log(myStr[5]);
+
+// String Immutability
+myStr = "Jello World";
+myStr[0] = "H";
+console.log(myStr); //Result --> Jello World
+// We can't change the letter by this
+
+//last letter in the string
+console.log(myStr[myStr.length-1]);
+
+//word blanks
+function wordBlanks(myNoun,myAdjective,myVerb,myAdverb){
+    var result ="";
+    result += "The " + myAdjective + " "+ myNoun +" "+  myVerb + " to the store " +myAdverb
+    return result;
+}
+
+console.log(wordBlanks('dog','big','ran','quickly'));
+console.log(wordBlanks('bike','slow','flew','slowly'));
+
+//Array
+var ourArray = ["John",23];
+var myArray = [];
+
+//Nested Array
+var multiArray = [['the universe',42],['everything',10110]];
+console.log(multiArray);
+
+//Access Array Data
