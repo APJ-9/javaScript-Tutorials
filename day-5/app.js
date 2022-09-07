985040b125780d34222d89305caa8430aacb8d97
APJ-9 <akhil.jose@vonnue.com>
2022-09-07 22:58:28 +0530

    today learned

---
+++ day-5/app.js
@@ -0,0 +1,13 @@
+//Access Multi-Dimensional Arrays with indexes
+var myArray = [[1,2,3],[4,5,6],[7,8,9],[[10,11,12],13,14]];
+var myData = myArray[3][0][1];//Result =11
+console.log(myData);
+
+//Manipulate Array with push()
+var ourArray = ['Akhil','J','cat'];
+ourArray.push(['happy','joy']);
+console.log(ourArray);
+var thisArray = [['John',23],['cat',2]];
+console.log(thisArray);
+
+//Manipulate Array with pop()
