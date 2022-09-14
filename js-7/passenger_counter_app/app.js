58060ec5a8ece19e12f2de671a58136bd18c332d
APJ-9 <akhil.jose@vonnue.com>
2022-09-14 11:16:22 +0530

    completed the passenger count app

--- js-7/passenger_counter_app/app.js
+++ js-7/passenger_counter_app/app.js
@@ -1,5 +1,20 @@
-// document.getElementById('count').innerText = 5;
-
 let count = 0;
-
-console.log(count);
+let total = 0;
+let countDisplay = document.getElementById('count-display');
+let previousDisplay = document.getElementById('previous-display');
+let totalDisplay = document.getElementById('total-count');
+function increment() {
+    count += 1;
+    // console.log("button has clicked");
+    countDisplay.innerText = count;
+    // console.log(count);
+}
+function save() {
+    // console.log('saved');
+    // console.log(count);
+    previousDisplay.textContent += count + ' - ';
+    total += count;
+    totalDisplay.innerHTML = total;
+    count = 0;
+    countDisplay.textContent = count;
+}
