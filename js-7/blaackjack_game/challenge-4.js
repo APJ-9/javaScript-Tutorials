58c3e432592492b1afaa7a871ee64df420134a3c
APJ-9 <akhil.jose@vonnue.com>
2022-09-14 19:58:08 +0530

    Completed challenges

---
+++ js-7/blaackjack_game/challenge-4.js
@@ -0,0 +1,9 @@
+let largeCountries = ['Tuvalu','India','USA','Indonesia','Monaco'];
+
+//  [TIME 4:23:42]
+
+//You need to help me fixup the largeCountries array so that
+//China and Pakistan are added back into their respective places
+
+//use push() & pop() and their counterparts unshift() & shift()
+//Google how to use unshift() and shift()
