9093ba9cbc7b77a9b0e012568168004e90301ca4
APJ-9 <akhil.jose@vonnue.com>
2022-09-15 18:45:25 +0530

    completed the chrome extensions

--- js-8/Leads-tracker/app.js
+++ js-8/Leads-tracker/app.js
@@ -1,4 +1,4 @@
-let myLeads = ['abc'];
+let myLeads = [];
 // myLeads.push('def');
 // myLeads = JSON.stringify(myLeads);
 // console.log(typeof(myLeads),myLeads);
@@ -9,30 +9,30 @@ const inputBtn = document.getElementById('input-btn');
 // localStorage.setItem("myName","Akhil P Jose");
 // console.log(localStorage.getItem("myName"));
 // localStorage.clear();
+const deleteBtn = document.getElementById('delete-btn');
+const tabBtn = document.getElementById('tab-btn');
 
-inputBtn.addEventListener('click', function () {
+// console.log(deleteBtn);
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-    let myData = inputEl.value;
-    myLeads.push(myData);
-    // console.log(myLeads);
-    // console.log(typeof(myData));
-    localStorage.setItem('myLeads', JSON.stringify(myLeads));
-    renderLeads();
-    let leadsFromLocalStorage = (localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    console.log(leadsFromLocalStorage);
+    render(myLeads);
+} else {
+    console.log('no items in the array');
+}
 
-    inputEl.value = '';
-})
-
-function renderLeads() {
+function render(leads) {
 
     let listItems = '';
 
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         // listItems += '<li><a target="_blank" href="' + myLeads[i] +'">' + myLeads[i] + '</li></a>';
         listItems += `
         <li>
-            <a target="_blank" href=" ${myLeads[i]} ">
-                ${myLeads[i]} 
+            <a target="_blank" href=" ${leads[i]} ">
+                ${leads[i]} 
             </a>
         </li>
         `;  //template strings or template literals
@@ -46,3 +46,34 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener('dblclick', function () {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
+
+inputBtn.addEventListener('click', function () {
+
+    let myData = inputEl.value;
+    myLeads.push(myData);
+    // console.log(myLeads);
+    // console.log(typeof(myData));
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    render(myLeads);
+    // localStorage.clear();
+    inputEl.value = '';
+});
+
+tabBtn.addEventListener('click', function () {
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        render(myLeads);
+
+    })
+    // console.log('hello world');
+    // let myTab = 'hello akhil';
+});
